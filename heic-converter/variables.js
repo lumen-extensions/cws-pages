7b5189c17c2f40a177fdb1808a93b5e174ace7fd
1day <1day@users.noreply.github.com>
2025-11-27 13:42:52 +0000

    deploy: lumen-extensions/heic-converter@8a9cd8ef84e71da1b7dc6b4f64b86ef01844eda0

---
+++ heic-converter/variables.js
@@ -0,0 +1,12 @@
+/**
+ * Extension variables for documentation pages
+ */
+const EXTENSION_VARIABLES = {
+  name: 'HEIC Converter',
+  version: '1.0.0',
+  googleFormUrl: 'https://docs.google.com/forms/d/e/1FAIpQLSe09yuzqXr0GHpjyFT54KdCAZoQdQGnHlgc_b2FlQHbJuf2uQ/viewform?embedded=true'
+};
+
+// Make variables available globally
+window.EXTENSION_VARIABLES = EXTENSION_VARIABLES;
+
